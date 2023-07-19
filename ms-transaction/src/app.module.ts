17e9b3b1870f22f35584b513758f939ea003cc3e
Jose Namoc <namoc.lopez.j@gmail.com>
2023-07-19 10:33:27 -0500

    added postgresql configuration

--- ms-transaction/src/app.module.ts
+++ ms-transaction/src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule]
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
