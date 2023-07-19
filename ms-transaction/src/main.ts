92cc47c9df8c63d2b43f977111c16a1471bdfb7f
Jose Namoc <namoc.lopez.j@gmail.com>
2023-07-19 10:02:43 -0500

    added basic configuration of kafka

--- ms-transaction/src/main.ts
+++ ms-transaction/src/main.ts
@@ -1,8 +1,54 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      consumer: {
+        groupId: 'kakfa-consumer',
+      },
+      client: {
+        brokers: [configService.get('KAFKA_BROKERS')],
+        ssl: true,
+        sasl: {
+          mechanism: 'plain',
+          username: configService.get('KAFKA_USERNAME'),
+          password: configService.get('KAFKA_PASSWORD'),
+        },
+      },
+    }
+  } as MicroserviceOptions)
+
+  app.startAllMicroservices()
+  
+  const options = new DocumentBuilder()
+  .setTitle('POC NestJS + Kakfa')
+  .setDescription('Documentation Proyect')
+  .setVersion('1.0')
+  .addTag('NestJS + KAFKA')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+
+  SwaggerModule.setup('api/docs', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true,
+    }
+  })
+
   await app.listen(3000);
 }
 bootstrap();
